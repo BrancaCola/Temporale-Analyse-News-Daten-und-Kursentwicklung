d780294fd8fdadacdfcc49c670ea03f4a8942ace
Johannes <jg39xobe@studserv.uni-leipzig.de>
2016-08-17 20:20:25 +0200

    clean up workspace

--- data/src/main/java/big/data/App.java
+++ data/src/main/java/big/data/App.java
@@ -43,29 +43,143 @@ public class App
 
 	public static void main( String[] args ) throws java.text.ParseException, IOException
 	{
-//		countTweets();
-//		unionTweets();
-		readTweets();
-//		tweetsHandler();
-//		countDates();
-//		googleTrends();
+		//count news per day
 //		countDatesReuters();
+//		countDatesCryptocoins();
+//		googleTrends();
+		
+		//read specific tweet file and count tweets per day
+//		countTweetsPerDay();
+		
+		//read jsons and save as one resource
+//		unionTweets();
+		
+		//read all tweets with sql and count them
+//		tweetsHandler();
     }
-    
-    public static void countTweets(){
-    	String FileName= "samples/tweets626.json";
-        try {
-        	//try to read the file
-            ArrayList<JSONObject> jsons=ReadJSON(new File(FileName),"UTF-8");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
-    
+
   
-    public static void readTweets() throws IOException{
+    //..a method which search the date in each line and count each day inkrementally
+	public static void countDatesReuters(){
+		String inputFileName = "samples/reuters_search_bitcoin.txt" ;
+	    String outputDirName = "output" ;
+	    SparkConf conf = new SparkConf().setAppName("org.sparkexample.WordCount").setMaster("local");
+	    JavaSparkContext context = new JavaSparkContext(conf);
+	    JavaRDD<String> file = context.textFile(inputFileName);
+	    //separates each line
+		JavaRDD<String> lines = file.flatMap(new FlatMapFunction<String, String>() {public Iterable<String> call(String s) { return Arrays.asList(s.split(System.getProperty("line.separator")));}});
+		//search specific format
+		JavaPairRDD<String, Integer> pairs = lines.mapToPair(new PairFunction<String, String, Integer>() {
+			public Tuple2<String, Integer> call(String s) throws java.text.ParseException { 
+				try{
+					SimpleDateFormat sd = new SimpleDateFormat("MMMM dd, yyyy", Locale.ENGLISH);
+					SimpleDateFormat sd2 = new SimpleDateFormat("yyyy-MM-dd");
+					Date specificDay = sd.parse(s);
+					String newDate = sd2.format(specificDay);
+					return new Tuple2<String, Integer>(newDate, 1);
+				}catch(Exception e){
+					return new Tuple2<String, Integer>("", 1);
+				}
+			}
+		});
+		JavaPairRDD<String, Integer> counts = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
+		  public Integer call(Integer a, Integer b) { return a + b; }
+		});
+	
+		counts.saveAsTextFile(outputDirName);
+				
+	}
+
+
+	//..a method which search the date in each line and count each day inkrementally
+	public static void countDatesCryptocoins(){
+		String inputFileName = "samples/cryptocoinsnews-pressreleases.txt" ;
+	    String outputDirName = "output" ;
+	    SparkConf conf = new SparkConf().setAppName("org.sparkexample.WordCount").setMaster("local");
+	    JavaSparkContext context = new JavaSparkContext(conf);
+	    JavaRDD<String> file = context.textFile(inputFileName);
+	    //separates each line
+		JavaRDD<String> lines = file.flatMap(new FlatMapFunction<String, String>() {public Iterable<String> call(String s) { return Arrays.asList(s.split(System.getProperty("line.separator")));}});
+		//search specific format
+		JavaPairRDD<String, Integer> pairs = lines.mapToPair(new PairFunction<String, String, Integer>() {
+			public Tuple2<String, Integer> call(String s) { 
+				if(s.matches("\\d{2}/\\d{2}/\\d{4}")){
+					String newFormat = ""+s.charAt(6)+s.charAt(7)+s.charAt(8)+s.charAt(9)+"-"+s.charAt(3)+s.charAt(4)+"-"+s.charAt(0)+s.charAt(1);
+					return new Tuple2<String, Integer>(newFormat, 1);
+				}else return new Tuple2<String, Integer>("", 1);
+			}
+		});
+	
+		JavaPairRDD<String, Integer> counts = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
+		  public Integer call(Integer a, Integer b) { return a + b; }
+		});
+	
+		counts.saveAsTextFile(outputDirName);
+				
+	}
+
+
+	public static void googleTrends() throws FileNotFoundException, java.text.ParseException{
+		
+		//2004-01-04 - 2004-01-10,0
+		//lines like this show the trend at google --> for one week, starting with the first date and a relativ trend [%] after the ","
+		
+		String inputFileName = "samples/bitcoin_google_trends_report.txt";
+	    String outputDirName = "output" ;
+	    File f = new File(inputFileName);
+		Scanner s = new Scanner(f);
+		ArrayList<String> dates = new ArrayList<String>();
+		SimpleDateFormat firstWeekdayFormat = new SimpleDateFormat("yyyy-MM-dd");
+		
+		while(s.hasNextLine()){
+			String amount, date, line = s.nextLine();
+			if(line.length()==0)break;
+			date = line.substring(0, 10);
+			amount = line.substring(24,line.length());
+			Date sDate = new Date();
+			Date anotherDay = new Date();
+			sDate = firstWeekdayFormat.parse(date);
+			
+			dates.add(date+", "+amount);
+			//generating the following dates
+			Calendar c = Calendar.getInstance(); 
+			c.setTime(sDate); 
+			c.add(Calendar.DATE, 1);
+			anotherDay = c.getTime();
+			dates.add(firstWeekdayFormat.format(anotherDay)+", "+amount);
+			c.setTime(sDate); 
+			c.add(Calendar.DATE, 2);
+			anotherDay = c.getTime();
+			dates.add(firstWeekdayFormat.format(anotherDay)+", "+amount);
+			c.setTime(sDate); 
+			c.add(Calendar.DATE, 3);
+			anotherDay = c.getTime();
+			dates.add(firstWeekdayFormat.format(anotherDay)+", "+amount);
+			c.setTime(sDate); 
+			c.add(Calendar.DATE, 4);
+			anotherDay = c.getTime();
+			dates.add(firstWeekdayFormat.format(anotherDay)+", "+amount);
+			c.setTime(sDate); 
+			c.add(Calendar.DATE, 5);
+			anotherDay = c.getTime();
+			dates.add(firstWeekdayFormat.format(anotherDay)+", "+amount);
+			c.setTime(sDate); 
+			c.add(Calendar.DATE, 6);
+			anotherDay = c.getTime();
+			dates.add(firstWeekdayFormat.format(anotherDay)+", "+amount);    		
+		}
+		File output = new File("samples/googleTrend.txt");
+		
+		try{
+			listInDatei(dates, output);
+		}catch(Exception e){
+			
+		}
+		
+	}
+
+
+	public static void countTweetsPerDay() throws IOException{
     	boolean woPostprocessing = true;
     	SparkConf conf = new SparkConf().setAppName("org.sparkexample.WordCount").setMaster("local");
         JavaSparkContext context = new JavaSparkContext(conf);
@@ -133,90 +247,90 @@ public class App
 	public static void unionTweets(){
 		String f1, f2, f3, f4, f5, f6, f7, f8, f9, f10;
 		String[] files = new String [20];
-    	files[0]= "samples/tweets621.json";
-    	files[1]= "samples/tweets623.json";
-    	files[2]= "samples/tweets624.json";
-    	files[3]= "samples/tweets626.json";
-    	files[4]= "samples/tweets627.json";
-    	files[5]= "samples/tweets628.json";
-    	files[6]= "samples/tweets629.json";
-    	files[7]= "samples/tweets701.json";
-    	files[8]= "samples/tweets704.json";
-    	files[9]= "samples/tweets706.json";
-    	files[10]= "samples/tweets708.json";
-    	files[11]= "samples/tweets711.json";
-    	files[12]= "samples/tweets713.json";
-    	files[13]= "samples/tweets715.json";
-    	files[14]= "samples/tweets717.json";
-    	files[15]= "samples/tweets718.json";
-    	files[16]= "samples/tweets719.json";
-    	files[17]= "samples/tweets721.json";
-    	files[18]= "samples/tweets726.json";
-    	files[19]= "samples/tweets727.json";
-    	SparkConf conf = new SparkConf().setAppName("org.sparkexample.WordCount").setMaster("local");
-        JavaSparkContext context = new JavaSparkContext(conf);
+		files[0]= "samples/tweets621.json";
+		files[1]= "samples/tweets623.json";
+		files[2]= "samples/tweets624.json";
+		files[3]= "samples/tweets626.json";
+		files[4]= "samples/tweets627.json";
+		files[5]= "samples/tweets628.json";
+		files[6]= "samples/tweets629.json";
+		files[7]= "samples/tweets701.json";
+		files[8]= "samples/tweets704.json";
+		files[9]= "samples/tweets706.json";
+		files[10]= "samples/tweets708.json";
+		files[11]= "samples/tweets711.json";
+		files[12]= "samples/tweets713.json";
+		files[13]= "samples/tweets715.json";
+		files[14]= "samples/tweets717.json";
+		files[15]= "samples/tweets718.json";
+		files[16]= "samples/tweets719.json";
+		files[17]= "samples/tweets721.json";
+		files[18]= "samples/tweets726.json";
+		files[19]= "samples/tweets727.json";
+		SparkConf conf = new SparkConf().setAppName("org.sparkexample.WordCount").setMaster("local");
+	    JavaSparkContext context = new JavaSparkContext(conf);
 		SQLContext sqlC = new SQLContext(context);
 		
 		DataFrame all, input0, in0, in1;
 		input0 = sqlC.read().json(files[0]);
-        
-        // Print the schema
-        //input1.printSchema();
-        
+	    
+	    // Print the schema
+	    //input1.printSchema();
+	    
 		
 		//Registers this DataFrame as a temporary table using tweets as name.
-        input0.registerTempTable("tweets");
-        /*
-         * Select tweets based on the amount of Retweets
-        DataFrame topTweets = sqlC.sql("SELECT text, user.name, retweet_count FROM tweets ORDER BY retweet_count DESC LIMIT 10");
-        Row[] result = topTweets.collect();
-        for (Row row : result) {
-          System.out.println(row.get(1)+" schrieb: "+row.get(0)+" Retweets: "+row.get(2));
-        }
-        ..there are always 0 Retweets in the Tweets --> cannot be used here. The reason is that the tweets are streamed directly
-        */
-        
-        in0 = sqlC.sql("SELECT id, text as message, created_at as date, user.id as user_id, user.name as user, user.followers_count as followers FROM tweets");
-        
-        all = in0;
-        
-        for(int file = 1; file < files.length;file++){
-        	input0 = sqlC.read().json(files[file]);
-        	input0.registerTempTable("tweets");
-        	in1 = sqlC.sql("SELECT id, text as message, created_at as date, user.id as user_id, user.name as user, user.followers_count as followers FROM tweets");
-            all = all.unionAll(in1);
-        }
-        
-        DataFrameWriter dfWriter = all.write();
-        dfWriter.json("samples/allTweets");
-        System.exit(0);
-        
-        
-        
-        
-        /*
-         * Select Tweets count per user
-         
-        DataFrame topTweets = sqlC.sql("SELECT user.name, count(*) AS anzahl FROM tweets GROUP BY user.name ORDER BY anzahl DESC LIMIT 10");
-        Row[] result = topTweets.collect();
-        for (Row row : result) {
-          System.out.println(row.get(1)+" Tweets geschrieben von: "+row.get(0));
-        }
-        System.exit(0);
-        
-        
-        JavaRDD<String> topTweetText = topTweets.toJavaRDD().map(new Function<Row, String>() {
-            public String call(Row row) {
-              return row.getString(0);
-            }});
-        System.out.println(topTweetText.collect());
-        */
-    
-    }
-    
-    
-    public static void tweetsHandler(){
-    	String FileName= "samples/tweets612Test.json";
+	    input0.registerTempTable("tweets");
+	    /*
+	     * Select tweets based on the amount of Retweets
+	    DataFrame topTweets = sqlC.sql("SELECT text, user.name, retweet_count FROM tweets ORDER BY retweet_count DESC LIMIT 10");
+	    Row[] result = topTweets.collect();
+	    for (Row row : result) {
+	      System.out.println(row.get(1)+" schrieb: "+row.get(0)+" Retweets: "+row.get(2));
+	    }
+	    ..there are always 0 Retweets in the Tweets --> cannot be used here. The reason is that the tweets are streamed directly
+	    */
+	    
+	    in0 = sqlC.sql("SELECT id, text as message, created_at as date, user.id as user_id, user.name as user, user.followers_count as followers FROM tweets");
+	    
+	    all = in0;
+	    
+	    for(int file = 1; file < files.length;file++){
+	    	input0 = sqlC.read().json(files[file]);
+	    	input0.registerTempTable("tweets");
+	    	in1 = sqlC.sql("SELECT id, text as message, created_at as date, user.id as user_id, user.name as user, user.followers_count as followers FROM tweets");
+	        all = all.unionAll(in1);
+	    }
+	    
+	    DataFrameWriter dfWriter = all.write();
+	    dfWriter.json("samples/allTweets");
+	    System.exit(0);
+	    
+	    
+	    
+	    
+	    /*
+	     * Select Tweets count per user
+	     
+	    DataFrame topTweets = sqlC.sql("SELECT user.name, count(*) AS anzahl FROM tweets GROUP BY user.name ORDER BY anzahl DESC LIMIT 10");
+	    Row[] result = topTweets.collect();
+	    for (Row row : result) {
+	      System.out.println(row.get(1)+" Tweets geschrieben von: "+row.get(0));
+	    }
+	    System.exit(0);
+	    
+	    
+	    JavaRDD<String> topTweetText = topTweets.toJavaRDD().map(new Function<Row, String>() {
+	        public String call(Row row) {
+	          return row.getString(0);
+	        }});
+	    System.out.println(topTweetText.collect());
+	    */
+	
+	}
+
+
+	public static void tweetsHandler(){
+    	String FileName= "";
     	String outputDirName = "output" ;
     	
     	//define configuration
@@ -256,112 +370,7 @@ public class App
         
    
     }
-   //reads each JSON Object and save the date in a list
-    public static synchronized ArrayList<JSONObject> ReadJSON(File MyFile,String Encoding) throws FileNotFoundException, ParseException {
-        Scanner s=new Scanner(MyFile,Encoding);
-        ArrayList<JSONObject> json=new ArrayList<JSONObject>();
-        while(s.hasNext()){
-        	try{
-        		 JSONObject obj= (JSONObject) new JSONParser().parse(s.nextLine());
-                 json.add(obj);
-        	}catch(Exception e){
-        	}
-        }
-        ArrayList<String> dates1 = new ArrayList<String>();
-        for(JSONObject obj : json){
-        	//save each day in an ArrayList
-            dates1.add((String) obj.get("created_at"));
-        }
-        System.out.println(dates1);
-        return json;
-    }
-    
-    //..a method which search the date in each line and count each day inkrementally
-    public static void countDates(){
-    	String inputFileName = "samples/cryptocoinsnews-pressreleases.txt" ;
-        String outputDirName = "output" ;
-        SparkConf conf = new SparkConf().setAppName("org.sparkexample.WordCount").setMaster("local");
-        JavaSparkContext context = new JavaSparkContext(conf);
-        JavaRDD<String> file = context.textFile(inputFileName);
-        //separates each line
-		JavaRDD<String> lines = file.flatMap(new FlatMapFunction<String, String>() {public Iterable<String> call(String s) { return Arrays.asList(s.split(System.getProperty("line.separator")));}});
-		//search specific format
-		JavaPairRDD<String, Integer> pairs = lines.mapToPair(new PairFunction<String, String, Integer>() {
-			public Tuple2<String, Integer> call(String s) { 
-				if(s.matches("\\d{2}/\\d{2}/\\d{4}")){
-					String newFormat = ""+s.charAt(6)+s.charAt(7)+s.charAt(8)+s.charAt(9)+"-"+s.charAt(3)+s.charAt(4)+"-"+s.charAt(0)+s.charAt(1);
-					return new Tuple2<String, Integer>(newFormat, 1);
-				}else return new Tuple2<String, Integer>("", 1);
-			}
-		});
 
-		JavaPairRDD<String, Integer> counts = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
-		  public Integer call(Integer a, Integer b) { return a + b; }
-		});
-
-		counts.saveAsTextFile(outputDirName);
-				
-    }
-    
-    
-    public static void googleTrends() throws FileNotFoundException, java.text.ParseException{
-    	
-    	//2004-01-04 - 2004-01-10,0
-    	//lines like this show the trend at google --> for one week, starting with the first date and a relativ trend [%] after the ","
-    	
-    	String inputFileName = "samples/bitcoin_google_trends_report.txt";
-        String outputDirName = "output" ;
-        File f = new File(inputFileName);
-    	Scanner s = new Scanner(f);
-    	ArrayList<String> dates = new ArrayList<String>();
-    	SimpleDateFormat firstWeekdayFormat = new SimpleDateFormat("yyyy-MM-dd");
-    	
-    	while(s.hasNextLine()){
-    		String amount, date, line = s.nextLine();
-    		if(line.length()==0)break;
-    		date = line.substring(0, 10);
-    		amount = line.substring(24,line.length());
-    		Date sDate = new Date();
-    		Date anotherDay = new Date();
-    		sDate = firstWeekdayFormat.parse(date);
-    		
-    		dates.add(date+", "+amount);
-    		//generating the following dates
-    		Calendar c = Calendar.getInstance(); 
-    		c.setTime(sDate); 
-    		c.add(Calendar.DATE, 1);
-    		anotherDay = c.getTime();
-    		dates.add(firstWeekdayFormat.format(anotherDay)+", "+amount);
-    		c.setTime(sDate); 
-    		c.add(Calendar.DATE, 2);
-    		anotherDay = c.getTime();
-    		dates.add(firstWeekdayFormat.format(anotherDay)+", "+amount);
-    		c.setTime(sDate); 
-    		c.add(Calendar.DATE, 3);
-    		anotherDay = c.getTime();
-    		dates.add(firstWeekdayFormat.format(anotherDay)+", "+amount);
-    		c.setTime(sDate); 
-    		c.add(Calendar.DATE, 4);
-    		anotherDay = c.getTime();
-    		dates.add(firstWeekdayFormat.format(anotherDay)+", "+amount);
-    		c.setTime(sDate); 
-    		c.add(Calendar.DATE, 5);
-    		anotherDay = c.getTime();
-    		dates.add(firstWeekdayFormat.format(anotherDay)+", "+amount);
-    		c.setTime(sDate); 
-    		c.add(Calendar.DATE, 6);
-    		anotherDay = c.getTime();
-    		dates.add(firstWeekdayFormat.format(anotherDay)+", "+amount);    		
-    	}
-    	File output = new File("samples/googleTrend.txt");
-    	
-    	try{
-    		listInDatei(dates, output);
-    	}catch(Exception e){
-    		
-    	}
-    	
-    }
     //for saving the ArrayList into a file
     private static void listInDatei(List list, File datei) throws IOException {
         PrintWriter printWriter = null;
@@ -377,36 +386,6 @@ public class App
         } finally {
             if(printWriter != null) printWriter.close();
         }
-    } 
-  //..a method which search the date in each line and count each day inkrementally
-    public static void countDatesReuters(){
-    	String inputFileName = "samples/reuters_search_bitcoin.txt" ;
-        String outputDirName = "output" ;
-        SparkConf conf = new SparkConf().setAppName("org.sparkexample.WordCount").setMaster("local");
-        JavaSparkContext context = new JavaSparkContext(conf);
-        JavaRDD<String> file = context.textFile(inputFileName);
-        //separates each line
-		JavaRDD<String> lines = file.flatMap(new FlatMapFunction<String, String>() {public Iterable<String> call(String s) { return Arrays.asList(s.split(System.getProperty("line.separator")));}});
-		//search specific format
-		JavaPairRDD<String, Integer> pairs = lines.mapToPair(new PairFunction<String, String, Integer>() {
-			public Tuple2<String, Integer> call(String s) throws java.text.ParseException { 
-				try{
-					SimpleDateFormat sd = new SimpleDateFormat("MMMM dd, yyyy", Locale.ENGLISH);
-					SimpleDateFormat sd2 = new SimpleDateFormat("yyyy-MM-dd");
-					Date specificDay = sd.parse(s);
-					String newDate = sd2.format(specificDay);
-					return new Tuple2<String, Integer>(newDate, 1);
-				}catch(Exception e){
-					return new Tuple2<String, Integer>("", 1);
-				}
-			}
-		});
-		JavaPairRDD<String, Integer> counts = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
-		  public Integer call(Integer a, Integer b) { return a + b; }
-		});
-
-		counts.saveAsTextFile(outputDirName);
-				
     }
     
 }
